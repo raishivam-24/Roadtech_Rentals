3bd154dbcc4b51207032aee19119d512c8101762
raishivam-24 <shivamrai1716@gmail.com>
2026-06-24 17:49:11 +0530

    Add responsive hamburger menu to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,23 @@
+import { useState } from "react";
 import { Link, NavLink } from "react-router-dom";
 import { NAV_ITEMS } from "../data/machines";
 import "../css/navbar.css";
 
 export default function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false);
   return (
     <nav className="nav">
       <Link to="/" className="nav-logo">
         ⚙ <span>ROAD</span>TECH
         <span className="nav-logo-sub"> RENTALS</span>
       </Link>
-      <ul className="nav-links">
+      <button
+        className="hamburger"
+        onClick={() => setMenuOpen(!menuOpen)}
+      >
+        ☰
+      </button>
+      <ul className={`nav-links ${menuOpen ? "active" : ""}`}>
         {NAV_ITEMS.map((item) => (
           <li key={item.path}>
             <NavLink
